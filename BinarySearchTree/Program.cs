8e3b01d5718b3d3a2c322609c9e47716399b57af
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-09 20:15:01 +0530

    [Apoorva] Add . Welcome to Binary Search Tree

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree!");
+        }
+    }
+}
