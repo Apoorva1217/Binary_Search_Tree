4cfce1ebc44c622c5f89bc9239249f1caf9bb7c6
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-09 22:56:43 +0530

    [Apoorva] Add . Ability to create the binary tree and count elements in the tree

--- BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree.cs
@@ -8,6 +8,7 @@ namespace BinarySearchTree
     {
         Node<T> Root;
         Node<T> currentNodeValue;
+        int count = 0;
         public T NodeData { get; set; }
         public BinarySearchTree<T> LeftTree { get; set; }
         public BinarySearchTree<T> RightTree { get; set; }
@@ -18,21 +19,26 @@ namespace BinarySearchTree
             this.RightTree = null;
         }
        
+        /// <summary>
+        /// Insert item into binary search tree
+        /// </summary>
+        /// <param name="item"></param>
         public void Insert(T item)
         {
             if (Root == null)
             {
                 this.Root = new Node<T>(item);
                 this.currentNodeValue = Root;
+                count++;
                 return;
             }
-            if ((this.currentNodeValue.data.CompareTo(item)) > 0)
+            if (this.currentNodeValue.data.CompareTo(item) > 0)
             {
-                ///Checks if left node is null.
                 if (this.currentNodeValue.leftNode == null)
                 {
                     this.currentNodeValue.leftNode = new Node<T>(item);
                     this.currentNodeValue = Root;
+                    count++;
                 }
                 else
                 {
@@ -42,11 +48,11 @@ namespace BinarySearchTree
             }
             else
             {
-                ///Checks if right node is null.
                 if (this.currentNodeValue.rightNode == null)
                 {
                     this.currentNodeValue.rightNode = new Node<T>(item);
                     this.currentNodeValue = Root;
+                    count++;
                 }
                 else
                 {
@@ -54,12 +60,31 @@ namespace BinarySearchTree
                     Insert(item);
                 }
             }
-
         }
+
+        /// <summary>
+        /// count the number of elements in BST
+        /// </summary>
+        /// <returns></returns>
+        public int Size()
+        {
+            Console.WriteLine("Count of elements in Binary Search Tree: ");
+            return count;
+        }
+
+        /// <summary>
+        /// Get the root element
+        /// </summary>
+        /// <returns></returns>
         public Node<T> GetRoot()
         {
             return this.Root;
         }
+
+        /// <summary>
+        /// display elements in BST
+        /// </summary>
+        /// <param name="node"></param>
         public void Display(Node<T> node)
         {
             if (node != null)
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -8,11 +8,23 @@ namespace BinarySearchTree
         {
             Console.WriteLine("Welcome to Binary Search Tree!");
 
-            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(3);
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(25);
             binarySearchTree.Insert(56);
             binarySearchTree.Insert(30);
             binarySearchTree.Insert(70);
+            binarySearchTree.Insert(22);
+            binarySearchTree.Insert(40);
+            binarySearchTree.Insert(11);
+            binarySearchTree.Insert(3);
+            binarySearchTree.Insert(16);
+            binarySearchTree.Insert(60);
+            binarySearchTree.Insert(65);
+            binarySearchTree.Insert(63);
+            binarySearchTree.Insert(67);
+            binarySearchTree.Insert(95);
+
             binarySearchTree.Display(binarySearchTree.GetRoot());
+            Console.WriteLine(binarySearchTree.Size());
             
         }
     }
